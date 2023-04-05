1c84d52cb79e965d4117c5cf2049cbb2d2cb139f
Arya Vijayan <99739280+AryaV14@users.noreply.github.com>
2023-04-06 00:49:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ class Fighter():
             target.hp = 0
             target.alive = False
             target.death()
+            #instance of damage_text class, each damage stored into the group
         damagetext = damage_text(target.rect.centerx,target.rect.y,str(damage),red)       
         damage_text_group.add(damagetext)
         self.action = 1
